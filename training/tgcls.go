06ad7b2299cb9f3803a2ae2ad2c165d0c7e28fd8
Ross Guarino <rssguar@gmail.com>
2014-12-08 16:59:47 -0500

    Range packets in the clasification step

--- training/tgcls.go
+++ training/tgcls.go
@@ -5,26 +5,37 @@ import(
 	"fmt"
     "./base"
     "gopkg.in/mgo.v2"
-    _ "gopkg.in/mgo.v2/bson"
+    "gopkg.in/mgo.v2/bson"
     "net"
 )
 
-var configPath string
+var configPath  string
+var start       int
+var end         int
 
 func init() {
     const (
         defaultConfig   = "config.json"
         configUsage     = "the location of the configuration file"
+        defaultStart    = 0
+        startUsage      = "the packet number to start clasification"
+        defaultEnd      = 0
+        endUsage        = "the last packet number to classify"
     )
     flag.StringVar(&configPath, "config", defaultConfig, configUsage)
     flag.StringVar(&configPath, "c" , defaultConfig, configUsage+" (shorthand)")
+    flag.IntVar(&start, "start", defaultStart, startUsage)
+    flag.IntVar(&start, "s", defaultStart, startUsage +" (shorthand)")
+    flag.IntVar(&end, "end", defaultEnd, endUsage)
+    flag.IntVar(&end, "e", defaultEnd, endUsage+" (shorthand)")
 }
 
 func main(){
 	flag.Parse()
     _ = configPath
-	// Iterate through each one of the packets and assign it to flow.
 
+	// Iterate through each one of the packets and assign it to flow.
+    // Starting at start and ending at end.
 	session, err := mgo.Dial("127.0.0.1")
     if err != nil {
         panic(err)
@@ -40,11 +51,45 @@ func main(){
     convFlows := make(map[string][]*base.Flow) // Conversation -> Flows
     flowPackets := make(map[string][]*base.Packet) // Flows -> Packets
 
+    convC := db.C("conversations")
+    flowC := db.C("flows")
+    packetC := db.C("packets")
+
+    index := mgo.Index{
+        Key: []string{"flow"},
+        Unique: false,
+        DropDups: false,
+        Background: true,
+    }
+    err = packetC.EnsureIndex(index)
+    if err != nil{
+        panic(err)
+    }
+    index = mgo.Index{
+        Key: []string{"conversation"},
+        Unique: false,
+        DropDups: false,
+        Background: true,
+    }
+    err = flowC.EnsureIndex(index)
+    if err != nil{
+        panic(err)
+    }
     // Iterate through each packet, create flows if needed and
-    flowCount := 0
-    conversationCount := 0
+    flowCount, err := flowC.Count()
+    if err != nil{
+        panic(err)
+    }
+    flowStart := flowCount
+
+    conversationCount, err := convC.Count()
+    if err != nil{
+        panic(err)
+    }
+    convStart := conversationCount
+
     var packet base.Packet
-    iter := rawPacketC.Find(nil).Iter()
+    iter := rawPacketC.Find(bson.M{"number": bson.M{"$gte":start, "$lt":end}}).Iter()
     for iter.Next(&packet) {
         // Process packet
         ep1, ep2 := packet.Endpoints()
@@ -93,30 +138,7 @@ func main(){
         flowPackets[flowKey] = append(flowPackets[flowKey],&newPacket)
     }
 
-    convC := db.C("conversations")
-    flowC := db.C("flows")
-    packetC := db.C("packets")
-
-    index := mgo.Index{
-        Key: []string{"flow"},
-        Unique: false,
-        DropDups: false,
-        Background: true,
-    }
-    err = packetC.EnsureIndex(index)
-    if err != nil{
-        panic(err)
-    }
-    index = mgo.Index{
-        Key: []string{"conversation"},
-        Unique: false,
-        DropDups: false,
-        Background: true,
-    }
-    err = flowC.EnsureIndex(index)
-    if err != nil{
-        panic(err)
-    }
+    
 
     for key, conversation := range(conversations){
         conversation.Duration = conversation.Endpoint - conversation.Start
@@ -150,8 +172,8 @@ func main(){
             panic(err)
         }
     }
-
     // Find the duration for the flows and conversations
-    fmt.Printf("Flows: %d\n", flowCount)
-    fmt.Printf("Conversatiosn: %d\n", conversationCount)
+    fmt.Printf("Indexed %d packets\n", end-start)
+    fmt.Printf("Flows: %d(%dnew)\n", flowCount, flowCount- flowStart)
+    fmt.Printf("Conversatiosn: %d(%d new)\n", conversationCount, conversationCount-convStart)
 }
